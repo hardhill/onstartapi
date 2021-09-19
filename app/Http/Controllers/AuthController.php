e2c9740d6fb16ce99565b899d065faed22ac5c13
hardhill <hardhill@mail.ru>
2021-09-19 09:51:23 +0800

    registration, login, logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,15 +6,16 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     //
     public function registration(Request $request){
         $fields = $request->validate([
-            'name'=>'required|string',
-            'email'=>'required|string|email',
-            'password'=>'required|string|confirmed'
+            'name'=>'required|string|max:255',
+            'email'=>'required|string|email|max:255|unique:users,email',
+            'password'=>'required|string|confirmed|min:8'
         ]);
         
             $user = User::create([
@@ -26,8 +27,41 @@ class AuthController extends Controller
             $token = $user->createToken('onstart')->plainTextToken;
             $response = [
                 'user'=> $user,
-                'token' => $token
+                'access_token' => $token,
+                'token_type' => 'Bearer'
             ];
         return response($response,201);
+    }
+    public function login(Request $request){
+        $fields = $request->validate([
+            'email'=>'required|string|email',
+            'password'=>'required|string'
+        ]);
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response('Login invalid', 503);
+        }
+        $token = $user->createToken('onstart')->plainTextToken;
+        $response = [
+            'user'=> $user,
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ];
+        return response($response,200);
+    }
+
+    public function logout()
+    {
+        //auth()->user()->tokens()->delete();
+        Auth::user()->tokens()->delete();
+        return [
+            'message' => 'Tokens Revoked'
+        ];
+    }
+
+    public function me(Request $request)
+    {
+        return $request->user();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,13 +19,15 @@ Route::get('/test',function(){
     return response(['message'=>'Test OK'],200);
 });
 Route::post('/reg',[AuthController::class,'registration']);
+Route::post('/login',[AuthController::class,'login']);
 
 //protected
 Route::group(['middleware'=>['auth:sanctum']],function(){
-
+    Route::post('/me', [AuthController::class, 'me']);
+    Route::post('/logout',[AuthController::class, 'logout']);
 });
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
